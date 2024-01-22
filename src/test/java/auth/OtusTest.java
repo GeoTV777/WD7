cd4f4f203bbc1169c486799ef2c874067fffad3e
eJwtzMGqwjAQheF3yboLW+I1zVIE3Sgidh2SyVRDYyck6a0ivrtBujsc+P430wCY0pUGHJlkG9OAELiuAYRpNe9XbYuIvNW1MNo2wK0xf9ywioVI/85iXOh7Kam8pG53Uqe8OdbnqevOx4Ozq9O2Ez1c4LrbH/J8adARv5fUj6j8ClicQR0xljcB/Q5tH26UYTLegRrwVUXUVlK8lRGomhJGiQ/tfDVTHHpPc8H4DC5iUjozOU7efz5fBIhQ9A== <ivushka777@mail.ru>
2024-01-22 14:53:59 +0300

    22/01/24 лишние страницы

--- src/test/java/auth/OtusTest.java
+++ src/test/java/auth/OtusTest.java
@@ -26,14 +26,19 @@ public class OtusTest {
     private String password= System.getProperty("password");
     private WebDriver driver;
     private WaitTools waitTools;
-//    private WebDriverWait webDriverWait;
     private String baseUrl = System.getProperty("base.url");
 
     @BeforeEach
     public void init() {
         driver = new DriverFactory().create();
+        logger.info("Driver installation");
         waitTools = new WaitTools(driver);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
         driver.get(baseUrl);
+        logger.info("Open browser in maximized");
+
     }
 
         @AfterEach
@@ -45,75 +50,33 @@ public class OtusTest {
             }
     }
 
-            @Test
-            public void autorization() {
-                String signInButtonLocator = "//button[text()='Войти']";
-                WebElement signInButton = driver.findElement(By.xpath(signInButtonLocator));
+    @Test
+    public void autorization() {
+        String signInButtonLocator = "//button[text()='Войти']";
+        WebElement signInButton = driver.findElement(By.xpath(signInButtonLocator));
 
-                String signInPopapSelector = "#__PORTAL__ > div";
-                Assertions.assertTrue(waitTools.waitForCondition(ExpectedConditions.presenceOfElementLocated(By.cssSelector(signInPopapSelector))),"Error");
+        String signInPopapSelector = "#__PORTAL__ > div";
+        Assertions.assertTrue(waitTools.waitForCondition(ExpectedConditions.presenceOfElementLocated
+                (By.cssSelector(signInPopapSelector))),"Error:signInPopapSelector is not visible");
 
-                signInButton.click();
+        signInButton.click();
 
-                WebElement authPopapElement = driver.findElement(By.cssSelector(signInPopapSelector));
+        WebElement authPopapElement = driver.findElement(By.cssSelector(signInPopapSelector));
+        Assertions.assertTrue(waitTools.waitForCondition(ExpectedConditions.visibilityOf(authPopapElement)),
+                "Error: signInPopapSelector is not visible");
 
-                Assertions.assertTrue(waitTools.waitForCondition(ExpectedConditions.visibilityOf(authPopapElement)), "Error?");
+        driver.findElement(By.xpath("//input[@name]/..")).click();
+        driver.findElement(By.xpath("//input[@name]")).sendKeys(login);
 
-                driver.findElement(By.xpath("//input[@name]/..")).click();
-                driver.findElement(By.xpath("//input[@name]")).sendKeys(login);
+        driver.findElement(By.xpath("//input[@type='password']/..")).click();
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);
 
-                driver.findElement(By.xpath("//input[@type='password']/..")).click();
-                driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);
+        driver.findElement(By.xpath("//button[./*[text()='Войти']]")).click();
+        Assertions.assertTrue(waitTools.waitForCondition(ExpectedConditions.not
+                (ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(signInButtonLocator)))),
+                "Error: the element is present");
+//     Вывод в лог всех cookie
+        System.out.println("Cookies: " + driver.manage().getCookies());
 
-                driver.findElement(By.xpath("//button[./*[text()='Войти']]")).click();
-                Assertions.assertTrue(waitTools.waitForCondition(ExpectedConditions.not(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(signInButtonLocator)))),"Error33");
-
-
-            }
-
-//
-//    @BeforeEach
-//   private void driverMiximize() {
-//        driver = new DriverFactory().create();
-//        logger.info("Driver installation");
-//        waitTools = new WaitTools(driver);
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--start-maximized");
-//        driver = new ChromeDriver(options);
-////        driver.get(baseUrl);
-//        driver.get("https://otus.ru");
-//        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(30));
-//        logger.info("Opening the browser in full window mode");
-//    }
-//    @AfterEach
-//    public void driverStop() {
-//        if (driver != null) {
-//            logger.info("Close browser");
-//            driver.close();
-//            driver.quit();
-//        }
-//    }
-//    @Test
-//    public void openMaximaze() {
-////     Нажимаем кнопку Войти на главной странице
-//        WebElement elEnter = driver.findElement(By.xpath("//button[text()='Войти']"));
-//        elEnter.click();
-////     На странице регистрации находим и кликаем поле Электронная почта
-//        WebElement elFocus = driver.findElement(By.xpath("//input[@name]/.."));
-//        elFocus.click();
-////     Вводим email в поле Электронная почта
-//        WebElement elEmail = driver.findElement(By.xpath("//input[@name]"));
-//        elEmail.sendKeys("login");
-////     На странице регистрации находим и кликаем поле Пароль
-//        elFocus = driver.findElement(By.xpath("//input[@type='password']/.."));
-//        elFocus.click();
-////     Вводим пароль в поле Пароль
-//        WebElement elPassword = driver.findElement(By.xpath("//input[@type='password']"));
-//        elPassword.sendKeys("password");
-////     Нажимаем кнопку Войти
-//        elEnter = driver.findElement(By.xpath("//button[./*[text()='Войти']]"));
-//        elEnter.click();
-//        System.out.println("Cookies: " + driver.manage().getCookies());
-//    }
-
+        }
 }
